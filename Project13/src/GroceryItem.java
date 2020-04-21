cc8786eada6537543ddc97f79ec29219cecac8ca
staceygrasso <59070489+staceygrasso@users.noreply.github.com>
2020-04-21 18:46:50 -0400

    Add files via upload

---
+++ Project13/src/GroceryItem.java
@@ -0,0 +1,22 @@
+public class GroceryItem{
+
+   //TO DO: GroceryItem instance variable declarations go here
+
+
+
+  //TO DO: GroceryItem methods go here
+
+
+
+
+
+
+   //main for testing GroceryItem methods
+   public static void main(String [] args){
+      
+
+
+
+   }
+  
+}
---
+++ Project13/src/GroceryListProgram.java
@@ -0,0 +1,12 @@
+
+public class GroceryListProgram{
+
+   public static void main (String [] args) ) {
+    //This program keeps track of a grocery list, allowing the 
+    //user to add, remove and report on items in the list
+
+
+
+
+   }
+}
